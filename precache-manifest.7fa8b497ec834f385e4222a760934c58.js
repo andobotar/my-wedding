6654337c200be093233cd2401ed6add97c3e79a7
Andó Botár <andobotar@yahoo.com>
2020-02-16 16:04:52 +0100

    Updates

---
+++ precache-manifest.7fa8b497ec834f385e4222a760934c58.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb143780e1fcc4fb901da3db83c3ab54",
+    "url": "/andobotar/my-wedding/index.html"
+  },
+  {
+    "revision": "d5ba80878e5d8abc0f85",
+    "url": "/andobotar/my-wedding/static/css/main.44099696.chunk.css"
+  },
+  {
+    "revision": "cd1e07318a336d032bcd",
+    "url": "/andobotar/my-wedding/static/js/2.95113314.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/andobotar/my-wedding/static/js/2.95113314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ba80878e5d8abc0f85",
+    "url": "/andobotar/my-wedding/static/js/main.458298fb.chunk.js"
+  },
+  {
+    "revision": "cd0c4575350a5615893b",
+    "url": "/andobotar/my-wedding/static/js/runtime-main.4acee9ce.js"
+  },
+  {
+    "revision": "249edf935a34022ffb72505d117bd18a",
+    "url": "/andobotar/my-wedding/static/media/aisle.249edf93.jpg"
+  },
+  {
+    "revision": "f403bc1e838b64517b5321602869f90f",
+    "url": "/andobotar/my-wedding/static/media/android-logo.f403bc1e.png"
+  },
+  {
+    "revision": "5a96c9f597ed8f50f9a72dbd86ad3e7d",
+    "url": "/andobotar/my-wedding/static/media/apple-logo.5a96c9f5.png"
+  },
+  {
+    "revision": "589793e85faf75e8c8400b4a442680ce",
+    "url": "/andobotar/my-wedding/static/media/chairs.589793e8.jpg"
+  },
+  {
+    "revision": "c14613d6f1cd4da0b7155b8e3392ba94",
+    "url": "/andobotar/my-wedding/static/media/flowers.c14613d6.jpg"
+  },
+  {
+    "revision": "e86cb1f85c8d1e23e4c7125001d56d27",
+    "url": "/andobotar/my-wedding/static/media/holding-hands.e86cb1f8.jpg"
+  }
+]);
